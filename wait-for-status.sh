cad55d37fd4868cbad62f27c3ff5e4a6fe5f9d79
kuter <contact@devktr.pl>
2020-03-23 07:31:53 +0100

    :sparkles: add interval

--- wait-for-status.sh
+++ wait-for-status.sh
@@ -7,6 +7,7 @@ Usage:
     ${0##*/} [-t timeout] [-s status_code] url
     -t TIMEOUT      timeout in seconds [default: 10]
     -s STATUS_CODE  expected status_code [default: 200]
+    -i INTERVAL     retry in seconds after fail [default: 5]
     -h              show this help
 
 USAGE
@@ -15,6 +16,7 @@ USAGE
 
 
 TIMEOUT=10
+INTERVAL=5
 STATUS_CODE=200
 
 while [[ $# -gt 0 ]]
@@ -22,6 +24,14 @@ do
     case "$1" in
         *:* ) 
             URL=(${1})
+            shift 1
+            ;;
+        -i)
+            INTERVAL="$2"
+            shift 2
+            ;;
+        --interval=*)
+            INTERVAL="${1#*=}"
             shift 1
             ;;
         -s)
@@ -67,8 +77,9 @@ while [[ $(curl -o /dev/null -s -w "%{http_code}\n" $URL) -ne $STATUS_CODE ]]; d
     if [[ "$SECONDS" -gt "$TIMEOUT" ]]; then
         exit 1
     fi
-    sleep 5
-    SECONDS=$(expr $SECONDS + 5)
+    sleep $INTERVAL
+    echo "$URL does not return expected $STATUS_CODE status code, will retry in $INTERVAL seconds .."
+    SECONDS=$(expr $SECONDS + $INTERVAL)
 done
 
 exec "${COMMAND[@]}"
